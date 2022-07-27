abbdda7c51fb0be2b331dddebafda5b54c9c8609
Malik <79022015+MalikN95@users.noreply.github.com>
2022-07-28 00:34:08 +0300

    update

--- controllers/home.controllers.js
+++ controllers/home.controllers.js
@@ -3,28 +3,33 @@ const Task = require('../models/Task')
 let sortValueHelper = false
 
 module.exports.homePage = async function(req, res){
-    const {skip} = req.query
-    const sortParams = req.params.sort
-    let sortValue
-    if(sortValueHelper){
-        sortValue = -1
-    } else{
-        sortValue = 1
+    try{
+        const {skip} = req.query
+        const sortParams = req.params.sort
+        let sortValue
+        if(sortValueHelper){
+            sortValue = -1
+        } else{
+            sortValue = 1
+        }
+        if(!skip){
+            sortValueHelper = !sortValueHelper
+        }
+        const tasks = await Task.find()
+            .sort({[sortParams]: [sortValue]})
+            .skip(skip)
+            .limit(3)
+            .lean()
+        const tasksCount = await Task.find()
+        const paginationLenth = Math.ceil(tasksCount.length / 3)
+        res.render('index', {
+            isHome: true,
+            title: 'Главная страница',
+            tasks,
+            paginationLenth
+        })
     }
-    if(!skip){
-        sortValueHelper = !sortValueHelper
+    catch(err) {
+        console.log(err);
     }
-    const tasks = await Task.find()
-        .sort({[sortParams]: [sortValue]})
-        .skip(skip)
-        .limit(3)
-        .lean()
-    const tasksCount = await Task.find()
-    const paginationLenth = Math.ceil(tasksCount.length / 3)
-    res.render('index', {
-        isHome: true,
-        title: 'Главная страница',
-        tasks,
-        paginationLenth
-    })
 }
